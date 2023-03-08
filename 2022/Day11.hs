5aab1829bf9a55b7bf11bc98ced2d3403d5aad24
Adam Barber <barberadam995@gmail.com>
2023-03-08 23:05:11 +0800

    Coming back to some of the Part 2 solutions

--- 2022/Day11.hs
+++ 2022/Day11.hs
@@ -14,6 +14,7 @@ import Control.Lens.Setter
 import Control.Lens.TH
 import Control.Monad.State.Strict
 import Control.Monad.State.Class
+import Control.Monad.Trans.Writer.Strict
 import Control.Monad
 import Data.Function
 import Data.List.Extra (splitOn)
@@ -22,27 +23,34 @@ import qualified Data.IntMap as M
 import Data.List (uncons, sort)
 import Data.Traversable (for, traverse)
 import Data.Foldable (foldl', foldMap', for_, traverse_)
-import Debug.Trace ( traceShow, trace )
+import Debug.Trace ( traceShow, trace, traceM )
 
 data Monkey = Monkey { _monkeyId :: Int,
-                       _startingItems :: [Int],
-                       _operation :: Int -> Int,
-                       _divisibleBy :: Int,
+                       _startingItems :: [Integer],
+                       _operation :: Integer -> Integer,
+                       _divisibleBy :: Integer,
                        _trueThrowTo :: Int,
                        _falseThrowTo :: Int }
 makeLenses ''Monkey
 
 data MonkeyState = MonkeyState { _monkeyMap :: M.IntMap Monkey,
-                                 _counts :: M.IntMap Int }
+                                 _counts :: M.IntMap Integer }
 makeLenses ''MonkeyState
+
+instance Eq Monkey where
+  (==) m1 m2 = _startingItems m1 == _startingItems m2 && _monkeyId m1 == _monkeyId m2
 
 main :: IO ()
 main = do
   monkeys <- map parse . splitBlocks <$> readFile "2022/Day11.txt"
   let n = length monkeys
+  print n
   let monkeyMap = M.fromList $ zip [0..] monkeys
   let intMap = M.fromList $ zip [0..n] (replicate n 0)
-  putStrLn . show $ foldr (*) 1 . take 2 . reverse . sort . M.elems $ _counts (execState (replicateM_ 20 doRoundL) (MonkeyState monkeyMap intMap))
+  let initState = MonkeyState monkeyMap intMap
+  let divisor = product $ map _divisibleBy monkeys
+  print $ product . take 2 . reverse . sort . M.elems $ _counts (execState (replicateM_ 20 (doRoundL (worry1 3))) initState)
+  print $ product . take 2 . reverse . sort . M.elems $ _counts (execState (replicateM_ 10000 (doRoundL (worry2 divisor))) initState)
 
 instance Show Monkey where
   show (Monkey id si op db tt ft) = "id:" ++ show id ++ "|" ++
@@ -54,20 +62,24 @@ instance Show Monkey where
 instance Show MonkeyState where
   show (MonkeyState mm cts) = show mm <> show cts
 
-doRoundL :: MonadState MonkeyState m => m ()
-doRoundL = do
+isInitialState :: M.IntMap Integer -> Bool
+isInitialState im = all (==0) (M.elems im)
+
+doRoundL :: MonadState MonkeyState m => (Monkey -> Integer -> Integer) -> m ()
+doRoundL wfn = do
   keys <- uses monkeyMap M.keys
-  forM_ keys (\k -> inspectL k)
+  forM_ keys (\k -> inspectL wfn k)
 
 -- using lenses allows us to write the inspect function almost
 -- completely imperatively
-inspectL :: M.Key -> MonadState MonkeyState m => m ()
-inspectL k = do
+inspectL :: (Monkey -> Integer -> Integer) -> M.Key -> MonadState MonkeyState m => m ()
+inspectL worry k = do
   m@Monkey {..} <- uses monkeyMap (M.! k) -- uses is similar to gets in mtl
-  counts . at k . each += length _startingItems
+  counts . at k . each += toInteger (length _startingItems)
   -- {..} exposes all the fields of the record in this scope. Only _startingItems is used
-  for_ (_startingItems) (\i -> do
+  for_ _startingItems (\i -> do
     let worryLevel = worry m i
+--    traceM (show worryLevel)
     -- drop first held item
     monkeyMap . at k . each . startingItems %= maybe [] snd . uncons
     -- pass first held item off
@@ -75,8 +87,11 @@ inspectL k = do
     then monkeyMap . at _trueThrowTo . each . startingItems %= (++ [worryLevel])
     else monkeyMap . at _falseThrowTo . each . startingItems %= (++ [worryLevel]))
 
-worry :: Monkey -> Int -> Int
-worry Monkey {..} i = _operation i `div` 3
+worry1 :: Integer -> Monkey -> Integer -> Integer
+worry1 d Monkey {..} i = _operation i `div` d
+
+worry2 :: Integer -> Monkey -> Integer -> Integer
+worry2 d Monkey {..} i = _operation i `mod` d
 
 splitBlocks :: String -> [String]
 splitBlocks = splitOn "\n\n"
@@ -85,11 +100,11 @@ parse :: String -> Monkey
 parse block = foldr parseLn (Monkey 0 [] id 0 0 0) (lines block)
   where
     parseLn :: String -> Monkey -> Monkey
-    parseLn line b = case (words line) of
+    parseLn line b = case words line of
       ["Monkey",id] ->  b{_monkeyId = read (take 1 id)}
       ("Starting":_:arr) -> b{_startingItems = map (read . filter (/= ','))  arr}
-      ["Operation:", "new", "=", "old", s, "old"] -> b{_operation = \old -> (parseSign s) old old}
-      ["Operation:", "new", "=", "old", s, d] -> b{_operation = \old -> (parseSign s) old (read d)}
+      ["Operation:", "new", "=", "old", s, "old"] -> b{_operation = \old -> parseSign s old old}
+      ["Operation:", "new", "=", "old", s, d] -> b{_operation = \old -> parseSign s old (read d)}
       ["Test:", "divisible", "by", n] -> b{_divisibleBy = read n}
       ["If", "true:", "throw", "to", "monkey", i] -> b{_trueThrowTo = read i}
       ["If", "false:", "throw", "to", "monkey", i] -> b{_falseThrowTo = read i}
--- 2022/Day9.hs
+++ 2022/Day9.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RecordWildCards #-}
 module Day9 where
 
 import Prelude
@@ -5,19 +6,24 @@ import Linear.V2
 import Control.Arrow
 import Control.Monad.State
 import Data.List
+import qualified Data.Set as S
 import Data.Foldable (traverse_)
-import Debug.Trace (trace)
+import Debug.Trace
 
 data Direction = U | L | R | D
 
+data RopeState = RopeState { positions :: [V2 Int], visited :: S.Set (V2 Int) }
+
 example :: String
 example = "R 4\nU 4\nL 3\nD 1\nR 4\nD 1\nL 5\nR 2"
+
+example2 :: String
+example2 = "R 5\nU 8\nL 8\nD 3\nR 17\nD 10\nL 25\nU 20"
 
 type Input = [Direction]
 
 main = do
   contents <- readFile "2022/Day9.txt"
-  traverse print $ scanl (+) (V2 0 0) (parse contents)
   print $ solve $ parse contents
 
 readInt :: String -> Int
@@ -31,13 +37,18 @@ parse str = concatMap ((\(dir: count:_) ->
          "D" -> V2   1    0  -- down
          "U" -> V2 (-1)   0) . words) $ lines str  -- up arr
 
-solve :: [V2 Int] -> Int
-solve moves = let arr = flip execState [(V2 0 0)] $ traverse_ move (scanl (+) (V2 0 0) moves) in trace (show arr) length $ nub arr
+initStatePart1 = RopeState [V2 0 0] $ S.singleton (V2 0 0)
 
-move :: V2 Int -> State [V2 Int] ()
+initStatePart2 = RopeState [V2 0 0, V2 0 0, V2 0 0, V2 0 0, V2 0 0, V2 0 0, V2 0 0, V2 0 0, V2 0 0] $ S.singleton (V2 0 0)
+
+solve :: [V2 Int] -> Int
+solve moves = let rs = flip execState initStatePart2 $ traverse_ move (scanl (+) (V2 0 0) moves) in length . S.elems $ visited rs
+
+move :: V2 Int -> State RopeState ()
 move h = do
-  ts <- get
-  let tailPos = head ts in modify (follow h tailPos:)
+  RopeState{..} <- get
+  let positions' = tail $ scanl follow h positions
+  trace (show positions') put $ RopeState positions' $ S.insert (last positions') visited
   where
     follow :: V2 Int -> V2 Int -> V2 Int
     follow h t | sum ((h - t)^2) < 4 = t
