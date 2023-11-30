782ddc5efd55aa57dd7ec5b1557d6e86c2193077
Adam Barber <barberadam995@gmail.com>
2023-11-30 20:54:59 +0000

    both parts of Day 5 passing

--- 2022/Day5.hs
+++ 2022/Day5.hs
@@ -2,9 +2,6 @@ module Day5 where
 
 import Prelude
 import qualified Data.Map as M
-import Control.Monad.Trans.Writer.CPS
-import Control.Monad.State
-import Control.Monad
 import Debug.Trace
 
 type Move = (Int, Int, Int)
@@ -12,12 +9,11 @@ type StackState = M.Map Int [String]
 
 main :: IO ()
 main = do
-  str <- readFile "Day5.txt"
+  str <- readFile "2022/Day5.txt"
   let (stacks, ms) = parseAll str
-  print stacks
-  print ms
-  let part1 = filter (/=']') . filter (/='[') $ describe $ moves stacks ms
-  print part1
+  let part m = filter (/=']') . filter (/='[') $ describe $ m stacks ms
+  print $ part moves1
+  print $ part moves2
 
 parseAll :: String ->  (StackState,  [Move])
 parseAll str = (parseStack first, parseMoves second)
@@ -29,7 +25,7 @@ parseStack :: [String] -> StackState
 parseStack ls = foldr parseStackLine M.empty (zip [1..] ls)
 
 parseStackLine ::  (Int, String) -> StackState -> StackState
-parseStackLine (i,l) state = foldl (\b (i,a) -> M.insertWith (++) i [a] b) state $ filter (\(i,a) -> a /= "   ") $ zip [1..] (fixedWords l)
+parseStackLine (i,l) state = foldl (\b (i,a) -> M.insertWith (++) i [a] b) state $ filter (("   " /=) . snd) $ zip [1..] (fixedWords l)
 
 parseMoves :: [String] -> [Move]
 parseMoves = map parseMoveLine
@@ -50,14 +46,23 @@ parseMoveLine = go . words
 move :: StackState -> (Int, Int, Int) -> StackState
 move state (n, source, dest) = iterate (moveOnce (source, dest)) state !! n
 
+moveMultiple :: StackState -> (Int, Int, Int) -> StackState
+moveMultiple state (n, source, dest) = case M.lookup source state of
+  Just xs -> let (f,s) = splitAt n xs in
+    M.insertWith (++) dest f (M.insert source s state)
+  _ -> state
+
 moveOnce :: (Int, Int) -> StackState -> StackState
 moveOnce (source, dest) state = case M.lookup source state of
   Just (x:xs) -> M.insertWith (++) dest [x] (M.insert source xs state)
   Just [] -> state
   Nothing -> trace ("&&&&&" <> show state) state
 
-moves :: StackState -> [Move] -> StackState
-moves = foldl move
+moves1 :: StackState -> [Move] -> StackState
+moves1 = foldl move
+
+moves2 :: StackState -> [Move] -> StackState
+moves2 = foldl moveMultiple
 
 describe :: StackState -> String
 describe state = trace (show state) foldr ((<>) . safeHead) "" $ M.elems state
