c74613770cbe39534061a13188782fb2584dd145
Adam Barber <barberadam995@gmail.com>
2023-12-08 23:59:21 +0000

    Code to verify cycling.

--- 2023/Day8.hs
+++ 2023/Day8.hs
@@ -24,7 +24,7 @@ main = do
   str <- readFile "Day8.txt"
   let init = execState (traverse parseLine (lines str)) $ Network { instructions = "", nodes = M.empty, currentNodes = [], multiples=[] }
   let part1 = evalState walk init
-  let state2 = execState walkMultiple init
+  let state2 = execState (walkMultiple 4) init
   let part2 = foldr lcm 1 state2.multiples
   print part1
   print part2
@@ -51,17 +51,19 @@ walk = do
      put s'{currentNodes = catMaybes xs}
      pure (Just "ZZZ" `elem` xs)) (zip [1..] $ cycle s.instructions)
 
-walkMultiple :: State Network (Maybe (Int, Char))
-walkMultiple = do
+-- Run with n > 1 first to verify they are cycling. Then just apply LCM.
+walkMultiple :: Int -> State Network (Maybe (Int, Char))
+walkMultiple n = do
   s <- get
   let init = filter ((=='A') . last) (everySecond $ fst <$> M.keys s.nodes)
   put $ trace (show init) s{currentNodes = init}
+  -- Keep doing the calc until all routes have hit Z.
   firstM (\a -> do
      s' <- get
      let xs' = mapMaybe (\y -> M.lookup (y, snd a) s'.nodes) s'.currentNodes
      let multiples' = s'.multiples <> (fst a <$ filter ((=='Z') . last) xs')
      put s'{currentNodes = xs', multiples = multiples'}
-     pure (length multiples' == length xs')) (zip [1..] $ cycle s.instructions)
+     pure (length multiples' == n * length xs')) (zip [1..] $ cycle s.instructions)
 
 everySecond :: [a] -> [a]
 everySecond (a:b:xs) = a:everySecond xs
